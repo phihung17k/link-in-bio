aa77bd297d272da3a0a08f0881ddcb366b74adac
Hung Nguyen <hungnguyen@ankvn.com>
2023-04-13 17:19:58 +0700

    test deep link page id fb

--- lib/pages/bio_preview/widgets/bio_preview_item_widget.dart
+++ lib/pages/bio_preview/widgets/bio_preview_item_widget.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
+import 'package:url_launcher/url_launcher_string.dart';
 import '../../../models/item_model.dart';
 
 class BioPreviewItemWidget extends StatelessWidget {
@@ -7,27 +8,63 @@ class BioPreviewItemWidget extends StatelessWidget {
   final ItemModel? item;
   const BioPreviewItemWidget({super.key, required this.item});
 
-  Future<void> _launchUrl(BuildContext context) async {
-    String url = "${item!.category!.baseURL}${item!.url}";
-    Uri uri = Uri.parse(url);
+  // Future<void> _launchUrl(BuildContext context) async {
+  //   String url = "${item!.category!.baseURL}${item!.url}";
+  //   Uri uri = Uri.parse(url);
 
-    if (await canLaunchUrl(uri)) {
-      await launchUrl(uri, mode: LaunchMode.externalApplication);
-    } else {
-      // ignore: use_build_context_synchronously
-      if (!context.mounted) return;
-      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-        content: Text("Could not launch $uri"),
-        duration: const Duration(seconds: 1),
-      ));
-    }
+  //   if (await canLaunchUrl(uri)) {
+  //     await launchUrl(uri, mode: LaunchMode.externalApplication);
+  //   } else {
+  //     // ignore: use_build_context_synchronously
+  //     if (!context.mounted) return;
+  //     ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+  //       content: Text("Could not launch $uri"),
+  //       duration: const Duration(seconds: 1),
+  //     ));
+  //   }
+  // }
+
+  Future<void> _launchUrlV2(BuildContext context) async {
+    /// Example:
+    /// ```dart
+    /// final httpsUri = Uri(
+    ///     scheme: 'https',
+    ///     host: 'dart.dev',
+    ///     path: 'guides/libraries/library-tour',
+    ///     fragment: 'numbers');
+    /// print(httpsUri); // https://dart.dev/guides/libraries/library-tour#numbers
+    ///
+    /// final mailtoUri = Uri(
+    ///     scheme: 'mailto',
+    ///     path: 'John.Doe@example.com',
+    ///     queryParameters: {'subject': 'Example'});
+    /// print(mailtoUri); // mailto:John.Doe@example.com?subject=Example
+    /// ```
   }
 
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: () async {
-        await _launchUrl(context);
+        // await _launchUrl(context);
+        //2384251218489340
+        //109861992081824
+        // String appUrl = "fb://page/109861992081824";
+        // String appUrl = "fb://profile/100007134556052";
+        String appUrl = "fb://page/109861992081824";
+        String webUrl = 'https://www.facebook.com/phihung17k';
+
+        if (await canLaunchUrlString(appUrl)) {
+          await launchUrlString(appUrl);
+        } else if (await canLaunchUrlString(webUrl)) {
+          await launchUrlString(webUrl);
+        } else {
+          if (!context.mounted) return;
+          ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+            content: Text("Could not launch $appUrl or $webUrl"),
+            duration: const Duration(seconds: 1),
+          ));
+        }
       },
       child: Card(
         elevation: 5,
